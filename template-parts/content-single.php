c9ddc03fd75846d1be453e9e3248902666afb310
anshu <anshu.pathak@logicielsolutions.co.in>
2021-10-24 18:14:55 +0530

    update some content page layout

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -1,13 +1,3 @@
-<?php
-/**
- * Template part for displaying single posts.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package OnePress
- */
-
-?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
