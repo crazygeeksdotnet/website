8e5b9daefb56bc4b61c0343eac28a36a64bbba8f
Anoop Pathak <anooppathakk@gmail.com>
2021-10-31 21:29:52 +0530

    done some changes in comment page

--- comments.php
+++ comments.php
@@ -74,7 +74,7 @@ if ( post_password_required() ) {
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'onepress' ); ?></h2>
+			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation is open', 'onepress' ); ?></h2>
 			<div class="nav-links">
 
 				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'onepress' ) ); ?></div>
@@ -90,7 +90,7 @@ if ( post_password_required() ) {
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
 		?>
-		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'onepress' ); ?></p>
+		<p class="no-comments"><?php esc_html_e( 'Comments are  not closed yet.', 'onepress' ); ?></p>
 	<?php endif; ?>
 
 	<?php comment_form(); ?>
